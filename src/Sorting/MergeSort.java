6c1b07af12c662c89b71db3a902d0e0ca01aa8bf
madhav1399 <madhav.manthini@gmail.com>
2025-03-17 01:08:02 +0530

    adding MergeSort

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,49 @@
+package Sorting;
+import java.util.*;
+public class MergeSort {
+
+    public static void Merge(int[] arr, int l, int mid, int r){
+        int i=l, j = mid+1;
+        //int n = arr.length;
+        int[] temp = new int[r+1];
+        int temp_ind =0;
+        while(i<=mid && j<=r){
+            if(arr[i]<=arr[j]){
+                temp[temp_ind++] = arr[i];
+                i++;
+            }
+            else{
+                temp[temp_ind++] = arr[j];
+                j++;
+            }
+        }
+        while(i<=mid){
+            temp[temp_ind++] = arr[i++];
+        }
+        while(j<=r){
+            temp[temp_ind++] = arr[j++];
+        }
+        //i=l;
+        for(int k=l;k<=r;k++){
+            arr[k]=temp[k-l];
+        }
+    }
+    public void MSort(int[] arr, int l, int r){
+        //int n = arr.length;
+        if(l==r) return;
+        int mid = l+(r-l)/2;
+
+        MSort(arr,l,mid);
+        MSort(arr,mid+1,r);
+        Merge(arr,l,mid,r);
+    }
+
+    public static void main(String[] args){
+        int arr[] = {3,1,2,4,1,5,2,6,4};
+        int n = arr.length;
+        MergeSort ms = new MergeSort();
+        ms.MSort(arr,0,n-1);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
